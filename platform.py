18785f7c0075d1f3d8690cb19ecf80a6b733c4b7
Georgi Angelov <the.wizarda@gmail.com>
2022-10-11 16:52:48 +0300

    Add files via upload

--- platform.py
+++ platform.py
@@ -5,13 +5,15 @@ from os.path import join, dirname, exists
 from importlib.machinery import SourceFileLoader
 from platformio.managers.platform import PlatformBase
 
+F_NAME = 'framework-wizio-TEST'
+
 class WiziotestPlatform(PlatformBase):
     def is_embedded(self):
-        print('[---] is_embedded()')
+        #print('[---] is_embedded()')
         return True
 
     def get_boards(self, id_=None):
-        print('[---] get_boards()')
+        #print('[---] get_boards()')
         res = PlatformBase.get_boards(self, id_)
 
         #self.on_installed()
@@ -19,20 +21,17 @@ class WiziotestPlatform(PlatformBase):
         return res
 
     def get_package_type(self, name):
-        print('[---] get_package_type()', name)
-        return self.packages[name].get("type")
+        #print('[---] get_package_type()', name)
+        return self.packages[name].get('type')
 
-    def on_installed(self):
-        print('[---] on_installed( + )')   
-       
-        p = join( dirname( __file__ ), 'builder', 'frameworks', 'install.py' ).replace("\\", "/")
+    def on_installed(self):     
+        p = os.path.join( dirname( __file__ ), 'builder', 'frameworks', 'install.py' ).replace('\\', '/')
         if exists( p ):
-            print('[---] on_installed( EXIST )')
+            f_dir = os.path.join( self.config.get('platformio', 'core_dir'), 'packages', F_NAME ),
             name = 'module_' +  str( abs( hash( p ) ) )
             m = SourceFileLoader(name, p).load_module() 
-            m.dev_install()
+            m.dev_install( f_dir[0] )
         else:
-            print('[---] on_installed( NOT EXIST )')
-
-        print('[---] on_installed( - )') 
+            print('[---] on_installed( INSTALL NOT EXIST )')
+ 
  
