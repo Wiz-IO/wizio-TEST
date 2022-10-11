fa8c9227582e9c29a33fec4128a8a111705c4c06
Georgi Angelov <the.wizarda@gmail.com>
2022-10-11 14:18:00 +0300

    Add files via upload

--- platform.py
+++ platform.py
@@ -1,18 +1,21 @@
 from platformio.managers.platform import PlatformBase
+from builder.frameworks.install import dev_install
 
 class WiziotestPlatform(PlatformBase):
     def is_embedded(self):
-        print('[---] is_embedded')
+        print('[---] is_embedded()')
         return True
 
     def get_boards(self, id_=None):
-        print('[---] get_boards')
+        print('[---] get_boards()')
         res = PlatformBase.get_boards(self, id_)
         return res
 
     def get_package_type(self, name):
-        print('[---] get_package_type', name)
+        print('[---] get_package_type()', name)
         return self.packages[name].get("type")
 
     def on_installed(self):
-        print('[---] on_installed')    
+        print('[---] on_installed(+)')   
+        dev_install() 
+        print('[---] on_installed(+)') 
