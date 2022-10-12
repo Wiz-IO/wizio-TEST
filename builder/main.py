8cad6fd9ea4eef4016f21c226fdb93e0177d4020
Georgi Angelov <the.wizarda@gmail.com>
2022-10-13 00:04:32 +0300

    Add files via upload

--- builder/main.py
+++ builder/main.py
@@ -3,28 +3,29 @@
 from os.path import join
 from SCons.Script import (AlwaysBuild, DefaultEnvironment, Default)
 
-#print('[---] main.py')
-
 env = DefaultEnvironment()
 
 elf = env.BuildProgram()
+
+# TODO
+
 '''
-bin = env.ELF2BIN( join("$BUILD_DIR", "${PROGNAME}"), elf )
-prg = env.Alias( "buildprog", bin, [ env.VerboseAction("", "DONE") ] )
+bin = env.ELF2BIN( join('$BUILD_DIR', '${PROGNAME}'), elf )
+prg = env.Alias( 'buildprog', bin, [ env.VerboseAction('', 'DONE') ] )
 AlwaysBuild( prg )
 
-#print( env.Dump() )
-
-debug_tool = env.GetProjectOption("debug_tool")
+debug_tool = env.GetProjectOption('debug_tool')
 if None == debug_tool:
     Default( prg ) # TODO
 else:   
     Default( prg )
 
-upload = env.Alias("upload", prg, [ 
-        env.VerboseAction("$UPLOADCMD", "Uploading..."),
-        env.VerboseAction("", ""),
+upload = env.Alias('upload', prg, [ 
+        env.VerboseAction('$UPLOADCMD', 'Uploading...'),
+        env.VerboseAction('', ''),
     ]
 )
 AlwaysBuild( upload )
 '''
+
+#print( env.Dump() )
