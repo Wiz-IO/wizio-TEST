4aa22efd57578194a9a8c0476f10515603082b96
Georgi Angelov <the.wizarda@gmail.com>
2022-10-13 10:50:01 +0300

    Delete cmake.py

--- builder/frameworks/cmake.py
+++
@@ -1,8 +0,0 @@
-# LICENSE: WizIO 2022 Georgi Angelov
-
-from SCons.Script import DefaultEnvironment
-
-env = DefaultEnvironment()
-env.platform = 'cmake' # platform separating
-
-#print( env.Dump() )
