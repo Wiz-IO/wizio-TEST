b0f69fc45bfe58848729a8ffe5b1e86357871d02
Georgi Angelov <the.wizarda@gmail.com>
2022-10-13 10:32:31 +0300

    Add files via upload

--- platform.py
+++ platform.py
@@ -4,38 +4,28 @@ from os.path import join, dirname, exists
 from importlib.machinery import SourceFileLoader
 from platformio.managers.platform import PlatformBase
 
-FRAMEWORK = 'framework-wizio-TEST'
+FRAMEWORK_NAME = 'framework-wizio-TEST'
 
 class WiziotestPlatform(PlatformBase):
     # .platformio/penv/Lib/site-packages/platformio/platform/base.py
 
     def is_embedded(self):
-        #print('[===] is_embedded()')
         return True
 
     def get_boards(self, id_=None):
-        #print('[===] get_boards()')
         res = PlatformBase.get_boards(self, id_)
-        ### self.on_installed() # TODO: REMOVE THIS !!!
         return res
 
-    #def board_config(self, id_):
-    #def configure_project_packages(self, env, targets=None):
-    #def configure_default_packages(self, options, targets):
-
     def get_package_type(self, name):
-        #print('[===] get_package_type()') # framework, toolchain...
-        T = self.packages[name].get('type')
+        T = self.packages[name].get('type')# framework, toolchain...
         if 'framework' == T:
-            #print('[===]\tPackage is framework')
             self.on_installed()
         return T
 
     def on_installed(self):  
-        #print('[===] on_installed()')  
         p = join( dirname( __file__ ), 'builder', 'frameworks', 'install.py' ).replace('\\', '/')
         if exists( p ):
-            f_dir = join( self.config.get('platformio', 'core_dir'), 'packages', FRAMEWORK ),
+            f_dir = join( self.config.get('platformio', 'core_dir'), 'packages', FRAMEWORK_NAME ),
             name = 'module_' +  str( abs( hash( p ) ) )
             m = SourceFileLoader(name, p).load_module() 
             m.dev_install( f_dir[0] )
